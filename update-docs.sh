51d1da1cdefa6a04bbc47f462911ea40b9815493
Lorin Hochstein <lhochstein@netflix.com>
2017-02-10 09:29:52 -0800

    Build docs

---
+++ update-docs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "DEPLOY_DOCS=$DEPLOY_DOCS"
+if [[ $DEPLOY_DOCS != true ]]; then
+    echo "Not building docs"
+    exit 0
+fi
+
+echo "Building docs"
+
+# Install mkdocs
+virtualenv venv
+venv/bin/pip install mkdocs
+
+# Decrypt and load the ssh key
+openssl aes-256-cbc -K $encrypted_5704967818cd_key -iv $encrypted_5704967818cd_iv -in docKey.enc -out docKey -d
+chmod 0600 docKey
+eval `ssh-agent -s`
+ssh-add docKey
+
+# Push up to gh-pages
+venv/bin/mkdocs gh-deploy
+
+
